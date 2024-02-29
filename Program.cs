1839dd205c597ec5142ad1fcfb08d45830a6031e
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2024-02-29 19:30:54 +0200

    done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int sideLength = 5; // Довжина сторони квадрата
+        char symbol = '*'; // Символ для відображення квадрата
+        DrawSquare(sideLength, symbol);
+    }
+
+    static void DrawSquare(int sideLength, char symbol)
+    {
+        for (int i = 0; i < sideLength; i++)
+        {
+            for (int j = 0; j < sideLength; j++)
+            {
+                Console.Write(symbol + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
