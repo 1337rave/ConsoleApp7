a610319fa4c281d5c3ba06a01ba2ad16454cb875
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2024-02-29 19:31:38 +0200

    done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = 1221;
+        bool isPalindrome = PalindromeChecker.IsPalindrome(number);
+        Console.WriteLine($"Is {number} a palindrome? {isPalindrome}");
+    }
+}
+
+class PalindromeChecker
+{
+    public static bool IsPalindrome(int number)
+    {
+        string numberString = number.ToString();
+        int left = 0;
+        int right = numberString.Length - 1;
+
+        while (left < right)
+        {
+            if (numberString[left] != numberString[right])
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
