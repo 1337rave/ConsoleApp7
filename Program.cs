7948e821d5a6d56841dbc572f50b5cc7319052e4
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2024-02-29 19:33:21 +0200

    done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] originalArray = { 1, 2, 6, -1, 88, 7, 6 };
+        int[] filterArray = { 6, 88, 7 };
+
+        int[] resultArray = FilterArray(originalArray, filterArray);
+
+        Console.WriteLine("Результат роботи методу:");
+        foreach (int num in resultArray)
+        {
+            Console.Write(num + " ");
+        }
+    }
+
+    static int[] FilterArray(int[] originalArray, int[] filterArray)
+    {
+        return originalArray.Where(num => !filterArray.Contains(num)).ToArray();
+    }
+}
